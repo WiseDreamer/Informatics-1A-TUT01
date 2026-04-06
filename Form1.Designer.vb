cdb3b4762ecf2b246b6595d96a1d086c45373fbf
Thendo Bigman <141783824+WiseDreamer@users.noreply.github.com>
2026-04-07 00:22:38 +0200

    Add files via upload

---
+++ Form1.Designer.vb
@@ -0,0 +1,100 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.grdTut = New UJGrid.UJGrid()
+        Me.btnDisplay = New System.Windows.Forms.Button()
+        Me.btnTotal = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(67, 12)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(132, 99)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Button1"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'grdTut
+        '
+        Me.grdTut.FixedCols = 1
+        Me.grdTut.FixedRows = 1
+        Me.grdTut.Location = New System.Drawing.Point(221, 76)
+        Me.grdTut.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.grdTut.Name = "grdTut"
+        Me.grdTut.Scrollbars = System.Windows.Forms.ScrollBars.Both
+        Me.grdTut.Size = New System.Drawing.Size(566, 231)
+        Me.grdTut.TabIndex = 1
+        '
+        'btnDisplay
+        '
+        Me.btnDisplay.Location = New System.Drawing.Point(67, 135)
+        Me.btnDisplay.Name = "btnDisplay"
+        Me.btnDisplay.Size = New System.Drawing.Size(132, 66)
+        Me.btnDisplay.TabIndex = 2
+        Me.btnDisplay.Text = "Display"
+        Me.btnDisplay.UseVisualStyleBackColor = True
+        '
+        'btnTotal
+        '
+        Me.btnTotal.Location = New System.Drawing.Point(67, 224)
+        Me.btnTotal.Name = "btnTotal"
+        Me.btnTotal.Size = New System.Drawing.Size(132, 30)
+        Me.btnTotal.TabIndex = 3
+        Me.btnTotal.Text = "Display Totals"
+        Me.btnTotal.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(67, 276)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(132, 47)
+        Me.Button2.TabIndex = 4
+        Me.Button2.Text = "Button2"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.btnTotal)
+        Me.Controls.Add(Me.btnDisplay)
+        Me.Controls.Add(Me.grdTut)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents grdTut As UJGrid.UJGrid
+    Friend WithEvents btnDisplay As Button
+    Friend WithEvents btnTotal As Button
+    Friend WithEvents Button2 As Button
+End Class
---
+++ Form1.vb
@@ -0,0 +1,114 @@
+﻿Option Strict On
+Option Explicit On
+Public Class Form1
+    'Global Vars
+    'static
+    'Overriding
+    Private nC, nR As Integer
+    Private myArray(3, 4) As Integer
+    Private RowTotal(3) As Integer
+    Private RowAverage(3) As Double
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        nR = 3
+        nC = 4
+        Init()
+
+        myArray(1, 1) = 9
+        myArray(1, 2) = 12
+        myArray(1, 3) = 13
+        myArray(1, 4) = 67
+
+        myArray(2, 1) = 32
+        myArray(2, 2) = 23
+        myArray(2, 3) = 43
+        myArray(2, 4) = 32
+
+        myArray(3, 1) = 3
+        myArray(3, 2) = 9
+        myArray(3, 3) = 9
+        myArray(3, 4) = 9
+
+
+    End Sub
+
+    Private Sub PIG(ByVal r As Integer, ByVal c As Integer, ByVal t As String)
+        grdTut.Row = r
+        grdTut.Col = c
+        grdTut.Text = t
+    End Sub
+
+    Private Sub btnDisplay_Click(sender As Object, e As EventArgs) Handles btnDisplay.Click
+        For r As Integer = 1 To nR
+            For c As Integer = 1 To nC
+                PIG(r, c, CStr(myArray(r, c)))
+            Next c
+        Next r
+
+    End Sub
+
+    Private Sub btnTotal_Click(sender As Object, e As EventArgs) Handles btnTotal.Click
+        For r As Integer = 1 To nR
+            Dim Total As Integer = 0
+            For c As Integer = 1 To nC
+                Total += myArray(r, c)
+            Next c
+            RowTotal(r) = Total
+            RowAverage(r) = Total / nC
+            PIG(r, nC + 1, CStr(RowTotal(r)))
+            PIG(r, nC + 2, CStr(RowAverage(r)))
+        Next r
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Dim myArray(4) As Integer
+        myArray(1) = 2
+        myArray(2) = 4
+        myArray(3) = 5
+        myArray(4) = 9
+        Dim Sum As Integer = FindTotal(myArray, 4)
+        MsgBox(Sum)
+        Dim Total As Integer = 0
+        findSum(myArray, 4, Total)
+
+
+    End Sub
+
+    Private Function FindTotal(ByRef arr() As Integer, ByVal size As Integer) As Integer
+        Dim Total As Integer = 0
+        For i As Integer = 1 To size
+            Total += arr(i)
+        Next i
+        Return Total
+    End Function
+
+    Private Sub findSum(ByRef arr() As Integer, ByVal Size As Integer, ByRef Total As Integer)
+        For i As Integer = 1 To Size
+            Total += arr(i)
+        Next i
+    End Sub
+
+    Private Sub Init()
+        'Resizing the grid dimensions
+        grdTut.Rows = nR + 1
+        grdTut.Cols = nC + 3
+
+        'Label The grid:
+        For r As Integer = 1 To nR
+            PIG(r, 0, "Row " & CStr(r)) 'Concatenation(&)
+        Next r
+        For c = 1 To nC
+            PIG(0, c, "Column " & CStr(c))
+        Next c
+
+        PIG(0, nC + 1, "Total")
+        PIG(0, nC + 2, "Average")
+    End Sub
+
+
+    ' 2 3 4 5 0
+    ' 4 5 7 8 5
+    ' 7 8 6 11 5
+
+
+End Class
